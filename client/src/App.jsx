6a960bb8a8ee07e4c5c18b676bbf96dd06236b03
yehezkielt <yehezkieltatang1@gmail.com>
2024-04-18 13:44:10 +0700

    feat: check login google

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,38 @@
-import { useSelector, useDispatch } from "react-redux";
+// import { useSelector, useDispatch } from "react-redux";
+import LoginPage from "./pages/LoginPage.jsx"
+
+// import {
+//   increment,
+//   decrement,
+//   incrementByAmount,
+// } from "./features/counter/counterSlice.js";
 
 import {
-  increment,
-  decrement,
-  incrementByAmount,
-} from "./features/counter/counterSlice.js";
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LoginPage/>,
+    },
+  ]);
 
 export default function App() {
-  const counter = useSelector((store) => store.counter.value);
+//   const counter = useSelector((store) => store.counter.value);
 
-  const dispatch = useDispatch();
+//   const dispatch = useDispatch();
 
-  return (
-    <div>
-      <h4>Global Counter : {counter} </h4>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
-      <button onClick={() => dispatch(incrementByAmount(2))}>
-        Increment by 2
-      </button>
-    </div>
-  );
+  return <RouterProvider router={router} />
+//   return (
+//     <div>
+//       <h4>Global Counter : {counter} </h4>
+//       <button onClick={() => dispatch(increment())}>Increment</button>
+//       <button onClick={() => dispatch(decrement())}>Decrement</button>
+//       <button onClick={() => dispatch(incrementByAmount(2))}>
+//         Increment by 2
+//       </button>
+//     </div>
+//   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,10 @@
-// import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-import App from './App'
-import store from './app/store'
-import { Provider } from 'react-redux'
 
-// As of React 18
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(
-  <Provider store={store}>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
 )
